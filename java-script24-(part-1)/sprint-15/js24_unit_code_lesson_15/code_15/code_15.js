b66e726d9998892147509af02b558a6e2951d32f
alex1001A <alexiordanov1998@gmail.com>
2024-09-03 23:32:06 +0300

    sprint 15 material_2

--- java-script24-(part-1)/sprint-15/js24_unit_code_lesson_15/code_15/code_15.js
+++ java-script24-(part-1)/sprint-15/js24_unit_code_lesson_15/code_15/code_15.js
@@ -1,22 +1,45 @@
-// document.getElementById('h3').onclick = helloFn
-// document.getElementById('h3').onclick = byeFn
+document.querySelector('.one').onclick = function (e) {
+    console.log(e);
+    console.log('click');
+}
 
-// document.getElementById('h3').onclick = function () {
-//     helloFn()
-//     byeFn()
+document.querySelector('.two').onclick = function () {
+    console.log('click2');
+}
+
+document.querySelector('.two').ondblclick = () => {
+    console.log('click2');
+}
+
+document.querySelector('.two').oncontextmenu = () => {
+    console.log('contextMenu');
+    return false
+}
+
+// let w = 100
+// document.querySelector('.three').onmousemove = function() {
+//     this.style.width = w + 'px'
+//     w++
 // }
 
-
-function helloFn() {
-    console.log(this);
-    console.log('hi');
+document.querySelector('.three').onmouseenter = () => {
+    document.querySelector('.three').style.background = 'red'
 }
 
-function byeFn() {
-    console.log(this);
-    console.log('bye');    
+document.querySelector('.three').onmouseleave = () => {
+    document.querySelector('.three').style.background = 'green'
 }
 
-document.getElementById('h3').addEventListener('click', helloFn)
-document.getElementById('h3').addEventListener('click', byeFn)
-document.getElementById('h3').removeEventListener('click', helloFn)
+document.querySelector('.three').onmousedown = () => {
+    document.querySelector('.three').style.background = 'orange'
+}
+
+document.querySelector('.three').onmouseup = () => {
+    document.querySelector('.three').style.background = 'blue'
+}
+
+let p = 10
+document.querySelector('button').onclick = (e) => {
+    p++
+    document.querySelector('progress').value = p
+}
